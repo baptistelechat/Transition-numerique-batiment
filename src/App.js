bccf63ef7d8d79486fd97e18fc793ead2c552600
Baptiste LECHAT <baptiste.lechat@ynov.com>
2020-12-30 18:22:01 +0100

    💄 change favicon and title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import HeaderBar from './components/headerBar'
 import CardContainer from './components/cardContainer'
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Transition numérique du bâtiment</title>
+      </Helmet>
       <HeaderBar/>
       <CardContainer/>
     </div>
