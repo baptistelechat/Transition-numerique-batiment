858a32e51cd478839c9055a8745bee81dbb8de94
Baptiste LECHAT <baptiste.lechat@ynov.com>
2021-01-08 11:06:04 +0100

    🎁 publish #3

--- src/components/reactModal.js
+++ src/components/reactModal.js
@@ -129,6 +129,23 @@ export default function ReactModal(props) {
             </Paper>
           )
 
+          
+        case 3:
+          return (
+            <Paper style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} className={classes.paper}>
+              <ScrollArea className={classes.scrollbar}>
+                <ListItem className={classes.ListItem}>
+                  <ListItemIcon><CloseIcon onClick={handleClose} className={classes.iconHeader}/></ListItemIcon>
+                  <h2 id="simple-modal-title" className={classes.title}>#{props.id} - Recevez-vous encore beaucoup de plans dessinés à la main ?</h2>
+                </ListItem>
+                <p id="simple-modal-description" className={classes.tag}>
+                #Bâtiment #Numérique #Ynov
+                </p>
+                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6753248299760807936" height="1171" width={width} frameborder="0" allowfullscreen="" title="Post intégré"></iframe>
+              </ScrollArea>
+            </Paper>
+          )
+
         default:
           console.log('Error');
       }
