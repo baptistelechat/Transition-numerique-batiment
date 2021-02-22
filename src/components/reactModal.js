82a3d8bed62c275bd4e057ec9b1ad36f766464cd
Baptiste LECHAT <baptistelechat@outlook.fr>
2021-02-22 07:01:09 +0100

    🎁 publish #22

--- src/components/reactModal.js
+++ src/components/reactModal.js
@@ -433,6 +433,22 @@ export default function ReactModal(props) {
               </Paper>
             )
 
+            case 22:
+            return (
+              <Paper style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} className={classes.paper}>
+                <ScrollArea className={classes.scrollbar}>
+                  <ListItem className={classes.ListItem}>
+                    <ListItemIcon><CloseIcon onClick={handleClose} className={classes.iconHeader}/></ListItemIcon>
+                    <h2 id="simple-modal-title" className={classes.title}>#{props.id} -  Se former au BIM est très rapide.</h2>
+                  </ListItem>
+                  <p id="simple-modal-description" className={classes.tag}>
+                  #Bâtiment #Numérique #BIM #Construction #Ynov
+                  </p>
+                  <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6769495306284363776" height="1114" width={width} frameborder="0" allowfullscreen="" title="Post intégré"></iframe>
+                </ScrollArea>
+              </Paper>
+            )
+
         default:
           console.log('Error');
       }
