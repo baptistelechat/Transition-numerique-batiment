3f80732fe4b91646f75baccc3ea0b56326b97ec6
Baptiste LECHAT <baptistelechat@outlook.fr>
2021-02-26 07:07:53 +0100

    🎁 publish #24

--- src/components/reactModal.js
+++ src/components/reactModal.js
@@ -465,6 +465,22 @@ export default function ReactModal(props) {
               </Paper>
             )
 
+            case 24:
+            return (
+              <Paper style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} className={classes.paper}>
+                <ScrollArea className={classes.scrollbar}>
+                  <ListItem className={classes.ListItem}>
+                    <ListItemIcon><CloseIcon onClick={handleClose} className={classes.iconHeader}/></ListItemIcon>
+                    <h2 id="simple-modal-title" className={classes.title}>#{props.id} -  Avez-vous déjà fais l'expérience de la réalité augmentée, virtuelle ou mixte ?</h2>
+                  </ListItem>
+                  <p id="simple-modal-description" className={classes.tag}>
+                  #Bâtiment #Numérique #BIM #Construction #Ynov
+                  </p>
+                  <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6770946818663555072" height="1171" width={width} frameborder="0" allowfullscreen="" title="Post intégré"></iframe>
+                </ScrollArea>
+              </Paper>
+            )
+
         default:
           console.log('Error');
       }
