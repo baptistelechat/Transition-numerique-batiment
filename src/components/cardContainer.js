1fd0505e224287edf2ba36958a7db92ac5e47097
Baptiste LECHAT <baptistelechat@outlook.fr>
2021-03-10 07:47:56 +0100

    🎁 publish #29

--- src/components/cardContainer.js
+++ src/components/cardContainer.js
@@ -83,7 +83,7 @@ const CardContainer = () => {
   const paperList = () => {
     const cards = []
     const nbCard = 42
-    const nbCardEnabled = 28
+    const nbCardEnabled = 29
     for (let i = 1; i <= nbCard; i++) {
       cards.push(
         range(1,nbCardEnabled).includes(i) ?
