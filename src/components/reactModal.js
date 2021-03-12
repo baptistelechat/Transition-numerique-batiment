70f83451e4a49eb48c4ce4804bec176fa5e0dfdd
Baptiste LECHAT <baptistelechat@outlook.fr>
2021-03-12 07:12:28 +0100

    🎁 publish #30

--- src/components/reactModal.js
+++ src/components/reactModal.js
@@ -561,6 +561,22 @@ export default function ReactModal(props) {
               </Paper>
             )
 
+            case 30:
+            return (
+              <Paper style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} className={classes.paper}>
+                <ScrollArea className={classes.scrollbar}>
+                  <ListItem className={classes.ListItem}>
+                    <ListItemIcon><CloseIcon onClick={handleClose} className={classes.iconHeader}/></ListItemIcon>
+                    <h2 id="simple-modal-title" className={classes.title}>#{props.id} - La mise en place de solution en réalité augmentée, virtuelle ou mixte réduit le risque d'erreur lors de la réalisation d'un projet ?</h2>
+                  </ListItem>
+                  <p id="simple-modal-description" className={classes.tag}>
+                  #Bâtiment #VR #RA #3D #Construction #Ynov
+                  </p>
+                  <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6776021581362737152" height="1068" width={width} frameborder="0" allowfullscreen="" title="Post intégré"></iframe>
+                </ScrollArea>
+              </Paper>
+            )
+
         default:
           console.log('Error');
       }
